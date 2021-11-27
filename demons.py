91d9fd4e6b1f7305e07a463dbb64ae4ecda7d1df
WTFkiro <1592143357@qq.com>
2021-11-27 20:18:16 +0800

    变身动画基本完成

--- demons.py
+++ demons.py
@@ -14,19 +14,19 @@ def led_output(file):
                 color = frame[25+y*70,25+x*70]
             text = ('leds['+str(sum)+'] = CRGB('+str(color[2])+','+str(color[1])+','+str(color[0])+');\n')
             exam = color[0]+color[1]+color[2]
-            if abs(mem[x][y] - exam)>20:
+            if abs(mem[x][y] - exam)>10:
                 file.write(text)
             mem[x][y] = exam
             sum+=1
     file.write('FastLED.show();\n'+'delay(30);\n')
     file.close()
     
-cap = cv.VideoCapture('video/demons.mp4')
-file = 'output/led.txt'
+cap = cv.VideoCapture('video/spider1.mp4')
+file = 'output/slice/spider1.txt'
 frames_num=cap.get(cv.CAP_PROP_FRAME_COUNT)
 pbar = tqdm(total = frames_num)
 nowframe = 0
-mem = np.arange(64).reshape(8,8)
+mem = np.arange(700,764,1).reshape(8,8)
 while True:
     ret,frame = cap.read()
     led_output(file)
